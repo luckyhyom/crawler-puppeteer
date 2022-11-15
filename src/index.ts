4e0d0e36a7b25ed349fc41ff94518de8217cf22d
luckyhyom <bs_khm@naver.com>
2022-11-15 15:19:43 +0900

    chore: add response handler, dockerfile

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,28 @@ import * as collectorService from './service/collectorService.js';
 const app = express();
 const port = config.port;
 
+function resHandler(data: any) {
+    return {
+        data: data,
+        statusCode: 200,
+    }
+}
+
 // ChannelDto
 app.get('/channels/:channelId', async (req, res) => {
     const result = await channelsService.getOneByChannelId(req.params.channelId);
-    return res.send(result);
+    return res.send(resHandler(result));
 });
 
 app.get('/channels', async (req, res) => {
     const result = await channelsService.getManyByTitle(req.query.title as string);
-    res.send(result);
+    res.send(resHandler(result));
 });
 
 app.get('/history/nox', async (req, res) => {
     const key = req.query.key as string;
     const result = await collectorService.collectFromNox(key);
-    res.send(result);
+    res.send(resHandler(result));
 });
 
 app.post('/channel-history/all', async (req, res) => {
@@ -31,12 +38,12 @@ app.post('/channel-history/all', async (req, res) => {
 //GET /storage/v1/history/view-sub/:{channel-id} 2H
 app.get('/history/view-sub/:channelId', async (req, res) => {
     const history = await channelsService.getHistory(req.params.channelId);
-    return res.send(history);
+    return res.send(resHandler(history));
 });
 
 app.get('/history/revenues/:channelId', async (req, res) => {
     const history = await channelsService.getMonthlyRevenue(req.params.channelId);
-    return res.send(history);
+    return res.send(resHandler(history));
 });
 
 app.get('/channel-history/:id', async (req, res) => {
